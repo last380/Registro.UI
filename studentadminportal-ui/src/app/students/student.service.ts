cec4a41dceb927c2abf4499c9b874e041b0a4acc
last380 <vivekkr8235@gmail.com>
2021-11-08 01:24:09 +0530

    Final Commit

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { AddStudentRequest } from '../Models/api-models/add-student-request.model';
 import { Student } from '../Models/api-models/student.model';
 import { UpdateStudentRequest } from '../Models/api-models/update-student-request.model';
@@ -10,7 +11,7 @@ import { UpdateStudentRequest } from '../Models/api-models/update-student-reques
 })
 export class StudentService {
 
-  private baseApiUrl = 'https://localhost:44384'
+  private baseApiUrl = environment.baseApiUrl;
 
   constructor(private httpClient: HttpClient) {  }
 
